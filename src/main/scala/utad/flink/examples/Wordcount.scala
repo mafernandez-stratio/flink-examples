85747454a474ab72a0a3f1af57cda0fb07e8ba81
mafernandez-stratio <mafernandez@stratio.com>
2019-06-25 02:10:53 +0200

    Basic example working

---
+++ src/main/scala/utad/flink/examples/Wordcount.scala
@@ -0,0 +1,25 @@
+package utad.flink.examples
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.scala._
+
+object WordCount extends App {
+
+  val params = ParameterTool.fromArgs(args)
+
+  val env = ExecutionEnvironment.getExecutionEnvironment
+
+  env.getConfig.setGlobalJobParameters(params)
+
+  val text = env.readTextFile(params.get("input"))
+
+  val counts = text.flatMap(_.toLowerCase.split("\\W+").filter(_.startsWith("j")))
+    .map((_, 1))
+    .groupBy(0)
+    .sum(1)
+
+  counts.writeAsCsv(params.get("output"), System.lineSeparator, ",")
+  env.execute("WordCount Example")
+
+}
+
