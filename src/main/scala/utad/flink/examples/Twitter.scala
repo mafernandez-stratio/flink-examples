ee0c82b947972ac12c2ad8ca47d9daea3ac91c5e
mafernandez-stratio <mafernandez@stratio.com>
2019-07-02 01:08:27 +0200

    Twitter example

---
+++ src/main/scala/utad/flink/examples/Twitter.scala
@@ -0,0 +1,60 @@
+package utad.flink.examples
+
+import java.io.FileInputStream
+import java.util.{Properties, StringTokenizer}
+
+import org.apache.flink.api.common.functions.FlatMapFunction
+import org.apache.flink.api.scala._
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.twitter.TwitterSource
+import org.apache.flink.util.Collector
+
+object Twitter extends App {
+
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  val twitterCredentials = new Properties()
+  twitterCredentials.load(new FileInputStream("/Users/miguelangelfernandezdiaz/workspace/twitter.properties"))
+
+  env.setParallelism(1)
+
+  // get input data
+  val streamSource: DataStream[String] = env.addSource(new TwitterSource(twitterCredentials))
+
+  val tweets: DataStream[(String, Int)] = streamSource.flatMap(new SelectEnglishAndTokenizeFlatMap).keyBy(0).sum(1)
+
+  tweets.writeAsText(s"/tmp/flink/twitter/${System.currentTimeMillis}")
+
+  // execute program
+  env.execute("Twitter Streaming Example")
+}
+
+/**
+  * Deserialize JSON from twitter source
+  *
+  * Implements a string tokenizer that splits sentences into words as a
+  * user-defined FlatMapFunction. The function takes a line (String) and
+  * splits it into multiple pairs in the form of "(word,1)" ({{{ Tuple2<String, Integer> }}}).
+  */
+private class SelectEnglishAndTokenizeFlatMap extends FlatMapFunction[String, (String, Int)] {
+  lazy val jsonParser = new ObjectMapper()
+
+  override def flatMap(value: String, out: Collector[(String, Int)]): Unit = {
+    val jsonNode = jsonParser.readValue(value, classOf[JsonNode])
+    val hasText = jsonNode.has("text")
+
+    (hasText, jsonNode) match {
+      case (true, node) => {
+        val tokenizer = new StringTokenizer(node.get("text").asText())
+
+        while (tokenizer.hasMoreTokens) {
+          val token = tokenizer.nextToken().replaceAll("\\s*", "").toLowerCase()
+          if (token.nonEmpty) out.collect((token, 1))
+        }
+      }
+      case _ =>
+    }
+  }
+
+}
